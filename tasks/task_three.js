b5b76d1555075c1fdafeff7b85a0cb6f8c8cc68f
Chidubem <nwekedubemph@gmail.com>
2023-07-11 06:54:01 -0700

    second commit

--- tasks/task_three.js
+++ tasks/task_three.js
@@ -2,7 +2,7 @@
 let myArrayOne = ["apple", "banana", "orange", "kiwi"];
 
 // Use array indexing to access a specific element
-let specificElements = myArray[1]; // accessing the element at index 1 (banana)
+let specificElements = myArrayOne[1]; // accessing the element at index 1 (banana)
 
 // Modify the element value using assignment
 myArray[1] = "grape"; // changing the element at index 1 to "grape"
